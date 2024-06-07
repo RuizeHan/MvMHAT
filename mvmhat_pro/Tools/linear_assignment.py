351bb9a566099ae5e729ba94375d3688deed773a
Feifan Wang <104878652+Wangff1234@users.noreply.github.com>
2024-06-07 20:17:17 +0800

    Delete mvmhat_pro directory

--- mvmhat_pro/Tools/linear_assignment.py
+++
@@ -1,9 +0,0 @@
-from scipy.optimize import linear_sum_assignment as linear_assignment
-import numpy as np
-
-
-def sklearn_linear_assignment(cost_matrix):
-    indices = linear_assignment(cost_matrix)
-    indices = np.asarray(indices)
-    indices = np.transpose(indices)
-    return indices
